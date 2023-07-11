58a7e99542d55bc99433838197078148579f8d5c
Mark Hoffner <mark.c.hoffner@gmail.com>
2023-07-11 14:32:31 -0400

    FEATURE: Add email link

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,6 +3,7 @@ import { render, waitFor, fireEvent } from '@testing-library/react';
 
 const Home = jest.fn();
 const About = jest.fn();
+const mailIconMock = jest.fn();
 const footerMock = jest.fn();
 const routeDataMock = [
     {
@@ -17,45 +18,52 @@ const routeDataMock = [
     }
 ]
 
+const aboutDataMock = {
+    emailAddress: 'mockEmailAddress'
+}
+
 describe('App', () => {
 
     let App;
 
     beforeEach(async () => {
-        jest.doMock('../../util/route-data.js', () => routeDataMock)
-        jest.doMock('./Footer', () => footerMock)
+        jest.doMock('../../util/route-data.js', () => routeDataMock);
+        jest.doMock('../../util/about-data.js', () => aboutDataMock);
+        jest.doMock('./MailIcon', () => mailIconMock);
+        jest.doMock('./Footer', () => footerMock);
 
-        Home.mockReturnValue(<>homeMock</>)
-        About.mockReturnValue(<>aboutMock</>)
-        footerMock.mockReturnValue(<>footerMock</>)
+        Home.mockReturnValue(<>homeMock</>);
+        About.mockReturnValue(<>aboutMock</>);
+        mailIconMock.mockReturnValue(<>mailIconMock</>);
+        footerMock.mockReturnValue(<>footerMock</>);
 
         const obj = await import('./App.jsx');
         App = obj.default;
-    })
+    });
 
     afterEach(() => {
         jest.resetAllMocks();
-    })
+    });
 
     it('renders without crashing', () => {
         render(<App />);
-    })
+    });
 
     it('displays the title', async () => {
         render(<App />);
         await waitFor(() => expect(document.title).toBe('Mark C Hoffner'));
-    })
+    });
 
     it('displays default route data', () => {
         const { getByText } = render(<App />);
         expect(getByText(/homeMock/)).toBeTruthy();
-    })
+    });
 
     it('routes to route data option', async () => {
         const { getByText } = render(<App />);
         fireEvent.click(getByText('About'));
         await waitFor(() => expect(getByText(/aboutMock/)).toBeTruthy());
-    })
+    });
 
     it('routes back to default route data', async () => {
         const { getByText } = render(<App />);
@@ -63,7 +71,7 @@ describe('App', () => {
         await waitFor(() => expect(getByText(/aboutMock/)).toBeTruthy());
         fireEvent.click(getByText('Home'));
         await waitFor(() => expect(getByText(/homeMock/)).toBeTruthy());
-    })
+    });
 
     it('displays the footer at all times', async () => {
         const { getByText } = render(<App />);
@@ -71,5 +79,20 @@ describe('App', () => {
         fireEvent.click(getByText('About'));
         await waitFor(() => expect(getByText(/aboutMock/)).toBeTruthy());
         await waitFor(() => expect(getByText(/footerMock/)).toBeTruthy());
-    })
-})
+    });
+
+    it('displays email address text', () => {
+        const { getByText } = render(<App />);
+        expect(getByText(/mockEmailAddress/)).toBeTruthy();
+    });
+
+    it('displays mail icon', () => {
+        const { getByText } = render(<App />);
+        expect(getByText(/mailIconMock/)).toBeTruthy();
+    });
+
+    it('links to given email address', () => {
+        const { getAllByRole } = render(<App />);
+        expect(getAllByRole('link')[0].getAttribute('href')).toBe('mailto: mockEmailAddress');
+    });
+});
